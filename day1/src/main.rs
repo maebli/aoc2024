8c07cb2fae7bd033d6aa8d40ca7f4223fa5d7df9
Michael Aebli <doland@duck.com>
2025-02-26 22:18:45 +0100

    adding benching

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,4 +1,7 @@
-fn main() {
+#![feature(test)]
+extern crate test;
+
+fn run() -> (i32, usize) {
     let input = include_str!("input.txt");
     let numbers = input.split_whitespace();
 
@@ -9,7 +12,7 @@ fn main() {
         .map(|(_, n)| n.parse::<i32>().unwrap())
         .collect::<Vec<i32>>();
 
-    let mut numbers = input.split_whitespace();
+    let numbers = input.split_whitespace();
     let mut right = numbers
         .enumerate()
         .filter(|(i, _)| i % 2 == 1)
@@ -30,6 +33,20 @@ fn main() {
         .map(|&l| (l as usize) * right.iter().filter(|&&x| x == l).count())
         .sum();
 
-    println!("Result1: {}", result);
-    println!("Result2: {}", result2);
+    (result, result2)
+}
+
+fn main() {
+    run();
+}
+
+#[cfg(test)]
+mod benches {
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_run(b: &mut Bencher) {
+        b.iter(|| run());
+    }
 }
